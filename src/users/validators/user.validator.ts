941beee863792ca553ee1c288eb97fd534a05740
mfsilva <mfsilva@nelogica.com.br>
2023-12-21 08:30:39 -0300

    Add user-validator

---
+++ src/users/validators/user.validator.ts
@@ -0,0 +1,46 @@
+import {
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator'
+import { UserProps } from '../domain/entities/user.entity'
+import { ClassValidatorField } from '@/shared/domain/validators/class-validator-fields'
+
+export class UserRules {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  name: string
+
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  email: string
+
+  @MaxLength(100)
+  @IsString()
+  @IsNotEmpty()
+  password: string
+
+  @IsDate()
+  @IsOptional()
+  createdAt?: Date
+
+  constructor({ email, name, password, createdAt }: UserProps) {
+    Object.assign(this, { email, name, password, createdAt })
+  }
+}
+
+export class UserValidator extends ClassValidatorField<UserRules> {
+  validate(data: any): boolean {
+    return super.validate(new UserRules(data ?? {}))
+  }
+}
+
+export class UserValidatorFactory {
+  static create(): UserValidator {
+    return new UserValidator()
+  }
+}
