72eb8cc059c5c114cd2f08f7eaefdfa69b65762b
mfsilva <mfsilva@nelogica.com.br>
2024-01-05 07:59:45 -0300

    Adding user-in-memory repository

---
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -0,0 +1,28 @@
+import { ConflictError } from '@/shared/domain/errors/conflict-error'
+import { NotFoundError } from '@/shared/domain/errors/not-found-error'
+import { InMemoryRespository } from '@/shared/domain/repositories/in-memory.repository'
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserRepository } from '@/users/repositories/user.repository'
+
+export class UserInMemoryRepository
+  extends InMemoryRespository<UserEntity>
+  implements UserRepository
+{
+  async findByEmail(email: string): Promise<UserEntity> {
+    const entity = this.items.find(item => item.email === email)
+
+    if (!entity) {
+      throw new NotFoundError(`Entity not found using email ${email}`)
+    }
+
+    return entity
+  }
+
+  async emailExists(email: string): Promise<void> {
+    const entity = this.items.find(item => item.email === email)
+
+    if (entity) {
+      throw new ConflictError('Email address already used')
+    }
+  }
+}
