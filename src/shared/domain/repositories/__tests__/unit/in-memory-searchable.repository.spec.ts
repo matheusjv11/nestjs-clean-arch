ce695d850dd98ac18bcdbcf6dc045c9ebe81e4a3
mfsilva <mfsilva@nelogica.com.br>
2024-01-10 08:50:07 -0300

    Finish in searchable respository and creates its testing file

---
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -0,0 +1,42 @@
+import { Entity } from '@/shared/domain/entities/entity'
+import { InMemorySearchableRespository } from '../../in-memory-searchable.repository'
+
+type StubEntityProps = {
+  name: string
+  price: number
+}
+
+class StubEntity extends Entity<StubEntityProps> {}
+
+class StubInMemorySearchableRepository extends InMemorySearchableRespository<StubEntity> {
+  sortableFields: string[] = ['name']
+
+  protected async applyFilter(
+    items: StubEntity[],
+    filter: string | null,
+  ): Promise<StubEntity[]> {
+    if (!filter) {
+      return items
+    }
+
+    return items.filter(item => {
+      return item.props.name.toLowerCase().includes(filter.toLowerCase())
+    })
+  }
+}
+
+describe('InMemorySearchableRespository unit tests', () => {
+  let sut: StubInMemorySearchableRepository
+
+  beforeEach(() => {
+    sut = new StubInMemorySearchableRepository()
+  })
+
+  describe('applyFilter method', () => {})
+
+  describe('applySort method', () => {})
+
+  describe('applyPaginate method', () => {})
+
+  describe('search method', () => {})
+})
