9d1fc249620642d1947a1bf19a5c2677dc7827f3
mfsilva <mfsilva@nelogica.com.br>
2024-01-16 07:55:27 -0300

    Add testing file for bcrypt

---
+++ src/users/infrastructure/providers/hash-provider/__tests__/unit/bcryptjs-hash.provider.spec.ts
@@ -0,0 +1,31 @@
+import { BcryptjsHashProvider } from '../../bcryptjs-hash.provider'
+
+describe('BcryptHashProvider', () => {
+  let sut: BcryptjsHashProvider
+
+  beforeEach(() => {
+    sut = new BcryptjsHashProvider()
+  })
+
+  it('Should return an encrypted password', async () => {
+    const password = 'Password123'
+    const hash = await sut.generateHash(password)
+    expect(hash).toBeDefined()
+  })
+
+  it('Should return false on invalid password and hash comparison', async () => {
+    const password = 'Password123'
+    const hash = await sut.generateHash(password)
+
+    const result = await sut.compareHash('fake', hash)
+    expect(result).toBeFalsy()
+  })
+
+  it('Should return true on valid password and hash comparison', async () => {
+    const password = 'Password123'
+    const hash = await sut.generateHash(password)
+
+    const result = await sut.compareHash(password, hash)
+    expect(result).toBeTruthy()
+  })
+})
