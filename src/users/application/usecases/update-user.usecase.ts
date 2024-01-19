9bd1595f18908b625696126baeedb691bfc0302a
mfsilva <mfsilva@nelogica.com.br>
2024-01-19 07:26:43 -0300

    Add update user usecase and its testing file

--- src/users/application/usecases/update-user.usecase.ts
+++ src/users/application/usecases/update-user.usecase.ts
@@ -1,10 +1,12 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository'
 import { UserOutput, UserOutputMapper } from '../dtos/user-output'
 import { UseCase as DefaultUseCase } from '@/shared/application/use-case/use-case'
+import { BadRequestError } from '@/shared/application/errors/badrequest-error'
 
-export namespace GetUserUseCase {
+export namespace UpdateUserUseCase {
   export type Input = {
     id: string
+    name: string
   }
 
   export type Output = UserOutput
@@ -13,7 +15,13 @@ export namespace GetUserUseCase {
     constructor(private userRepository: UserRepository.Repository) {}
 
     async execute(input: Input): Promise<Output> {
+      if (!input.name) {
+        throw new BadRequestError('Name not provided')
+      }
+
       const entity = await this.userRepository.findById(input.id)
+      entity.update(input.name)
+      await this.userRepository.update(entity)
 
       return UserOutputMapper.toOutput(entity)
     }
