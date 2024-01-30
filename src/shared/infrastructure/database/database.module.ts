3748fc6a50cf9e6afdc65115586de344f30804b3
mfsilva <mfsilva@nelogica.com.br>
2024-01-30 09:17:02 -0300

    Integration test file for user respository on prisma

--- src/shared/infrastructure/database/database.module.ts
+++ src/shared/infrastructure/database/database.module.ts
@@ -1,11 +1,24 @@
-import { Module } from '@nestjs/common'
+import { DynamicModule, Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { ConfigService } from '@nestjs/config'
 import { EnvConfigModule } from '../env-config/env-config.module'
+import { PrismaClient } from '@prisma/client'
 
 @Module({
   imports: [EnvConfigModule.forRoot()],
   providers: [ConfigService, PrismaService],
   exports: [PrismaService],
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+  static forTest(prismaClient: PrismaClient): DynamicModule {
+    return {
+      module: DatabaseModule,
+      providers: [
+        {
+          provide: PrismaService,
+          useFactory: () => prismaClient as PrismaService,
+        },
+      ],
+    }
+  }
+}
