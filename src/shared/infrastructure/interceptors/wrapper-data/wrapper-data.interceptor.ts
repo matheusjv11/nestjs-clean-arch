87e7b703d5d3def41f16fd5ba24e8b108c0af19b
Matheus Almeida <matheusjv14@gmail.com>
2024-02-08 08:13:50 -0300

    Add Wrapper data interceptor

---
+++ src/shared/infrastructure/interceptors/wrapper-data/wrapper-data.interceptor.ts
@@ -0,0 +1,21 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common'
+import { Observable, map } from 'rxjs'
+
+export class WrapperDataInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> {
+    return next.handle().pipe(
+      map(body => {
+        if (!body || 'meta' in body) {
+          return body
+        }
+
+        return {
+          data: body,
+        }
+      }),
+    )
+  }
+}
